dde3197ec105932cb994d08ce4f8024547e62445
adchi1 <ada_anya@snu.ac.kr>
2026-01-04 23:27:35 -0500

    I added the title and subtitle to the homepage as components.

--- react-crashcourse/App.tsx
+++ react-crashcourse/App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface TitleProps {
+  title: string;
+  subtitle: string;
+}
+
+// Use the interface for type-checking the props
+const TitleComponent: React.FC<TitleProps> = ({ title, subtitle }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </div>
+  );
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <TitleComponent
+        title="Staff Check-in"
+        subtitle="Ada Anya"
+      />
     </View>
   );
 }
